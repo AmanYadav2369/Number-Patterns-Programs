90021b32066ef11066e4b22f8db07291f55ace97
AmanYadav2369 <130037525+AmanYadav2369@users.noreply.github.com>
2023-04-20 15:43:17 +0530

    Add files via upload

---
+++ Number Patterns/p3.java
@@ -0,0 +1,14 @@
+class p3
+{
+public static void main(String args[])
+{
+int count=0;
+for(int i=1; i<=4; i++){
+for(int j=1; j<=i; j++){
+count=count+1;
+System.out.print(count);
+}
+System.out.println();
+}
+}
+}
